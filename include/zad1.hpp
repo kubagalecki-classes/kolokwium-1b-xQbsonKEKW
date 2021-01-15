e801ad678f3f6800bc1fbfcbd90a3e4f2f17ef8b
xQbsonKEKW <jakubszczuka@gmail.com>
2021-01-15 08:08:44 +0000

    zad2 ver3

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -8,14 +8,16 @@
 
 using namespace std;
 // tutaj klasa Artysta
-class Artysta 
+class Artysta
 {
+private:
     string pseudonim;
     unsigned sluchacze;
 public:
     Artysta() { this->pseudonim = "Anonim"; this->sluchacze = 0; }
     Artysta(const std::string& a, unsigned b) : pseudonim(a), sluchacze(b) {}
-    void graj(std::ostream& a) const {
+    ~Artysta() {};
+    virtual void graj(std::ostream& a) const {
         a << this->pseudonim << ": " << this->sluchacze;
     }
     const std::string& getPseudonim() const
--- include/zad2.hpp
+++ include/zad2.hpp
@@ -6,27 +6,7 @@
 
 using namespace std;
 // tutaj klasa Artysta
-class Artysta
-{
-private:
-    string pseudonim;
-    unsigned sluchacze;
-public:
-    Artysta() { this->pseudonim = "Anonim"; this->sluchacze = 0; }
-    Artysta(const std::string& a, unsigned b) : pseudonim(a), sluchacze(b) {}
-    ~Artysta() {};
-    virtual void graj(std::ostream& a) const {
-        a << this->pseudonim << ": " << this->sluchacze;
-    }
-    const std::string& getPseudonim() const
-    {
-        return this->pseudonim;
-    }
-    unsigned getSluchacze() const
-    {
-        return this->sluchacze;
-    }
-};
+
 
 class PopGwiazda : public Artysta
 {
