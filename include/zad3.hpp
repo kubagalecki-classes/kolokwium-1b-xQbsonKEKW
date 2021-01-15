b0d4feba9a32d674d1dd3a7976cc0c30ef27fb21
xQbsonKEKW <jakubszczuka@gmail.com>
2021-01-15 08:55:54 +0000

    zad3 i znowu

--- include/zad3.hpp
+++ include/zad3.hpp
@@ -10,7 +10,10 @@ private:
     Artysta a;
     T i;
 public:
-    ArtystaPlusInstrument(const Artysta& _a, const T& _i) : a(_a), i(_i) {}
+    ArtystaPlusInstrument(const Artysta& _a, const T& _i){
+      a = _a;
+      i = _i; 
+    }
     unsigned koncert(){
       return i.liczbaUczestnikow(a);
       }
