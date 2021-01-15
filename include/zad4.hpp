09953fcee972e99172109f13ee25d444d352362e
xQbsonKEKW <jakubszczuka@gmail.com>
2021-01-15 08:41:48 +0000

    zad 4 ver 1

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -4,3 +4,14 @@
 #include <vector>
 
 // tutaj funkcja stworzZespol
+std::vector<Artysta> stworzZespol(int a, const std::string& str)
+{
+    std::vector<Artysta> vec;
+    if (str.size() = a)
+        throw std::logic_error("error");
+    
+    for(int i = 1; i < a+1; i++)
+        vec.push_back(Artysta{str, i});
+    
+    return vec;
+}
