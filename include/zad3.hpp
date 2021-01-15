a9a952e8a9265e2e6c0f2067cdd0e989389cf79b
xQbsonKEKW <jakubszczuka@gmail.com>
2021-01-15 08:17:47 +0000

    zad 3 ver 1

--- include/zad3.hpp
+++ include/zad3.hpp
@@ -3,3 +3,15 @@
 #include "zad1.hpp"
 
 //tutaj szablon ArtystaPlusInstrument
+template <class T>
+class ArystaPlusInstrument {
+private:
+    Artysta a;
+    T i;
+public:
+    ArystaPlusInstrument() {};
+    ArystaPlusInstrument(const Artysta& ar, const T& tt) : a(ar),i(tt) {};
+    usigned koncert() {
+        return this->i.liczbaUczestnikow(a);
+    }
+};
