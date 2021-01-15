83676dda438ac4867c76757f29e61e6da6b833b3
xQbsonKEKW <jakubszczuka@gmail.com>
2021-01-15 07:27:06 +0000

    zad1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -7,3 +7,19 @@
 #include <string>
 
 // tutaj klasa Artysta
+class Artysta {
+public:
+    Artysta() { this->pseudonim = "Anonim"; }
+    Artysta(const string& str, unsigned P) : pseudonim(str), sluchacze(P) {};
+    ~Artysta() {};
+
+    void graj(ostream& os) const {
+        os << this->pseudonim << " : " << this->sluchacze;
+    }
+    const string& getPseudonim() const { return this->pseudonim; }
+    unsigned getSluchacze() const { return this->sluchacze; }
+private:
+    std::string pseudonim;
+    unsigned sluchacze;
+
+};
