7783eb1bed65241b72596eed9a7c228e856a6af6
xQbsonKEKW <jakubszczuka@gmail.com>
2021-01-15 08:51:15 +0000

    zad 5 ver 1

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -5,3 +5,7 @@
 #include <vector>
 
 // tutaj algorytm liczPopularnych(T begin, T end)
+template<typename T>
+unsigned liczPopularnych(T t1, T t2){
+    return std::count_if(t1, t2, [](const Artysta& ar){return ar.getSluchacze() > 1000;});
+}
